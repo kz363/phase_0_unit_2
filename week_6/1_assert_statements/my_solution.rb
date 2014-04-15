6ecbf9dba2de54e6d6f4c72445d44a129ad3daaf
kz363 <kz363@nyu.edu>
2014-04-15 17:10:23 -0400

    finished challenge 1/2

--- week_6/1_assert_statements/my_solution.rb
+++ week_6/1_assert_statements/my_solution.rb
@@ -16,17 +16,46 @@ assert { name == "billybob" }
 
 # 2. Pseudocode what happens when the code above runs
 
-
+# yield becomes true/false depending on what's inside the block after assert is called. In this case, the first 
+# assert will run fine but the second one will raise Assertion Failed.
 
 # 3. Copy your selected challenge here
 
+class RPNCalculator
 
+  def evaluate(rpn_expression)
+    rpn_array = rpn_expression.split(" ").map!{|i| i =~ /\d/ ? i.to_i : i.to_sym} # Converts the string to an array where each element is either an integer or a symbol
+    until rpn_array.length == 1 # Keeps looping until it evaluates everything in the expression.
+      rpn_array.each_with_index do |i, index|
+        if rpn_array[index+2] =~ /[-+*]/ # Looks 2 elements ahead for an operator
+          rpn_array[index..index+2] = rpn_array[index..index+1].inject(rpn_array[index+2])
+          break
+        end
+      end
+    end
+    rpn_array[0]
+  end
 
+end
 
 # 4. Convert your driver test code from that challenge into Assert Statements
 
+rpn = RPNCalculator.new
+assert { rpn.evaluate('0') == 0 }
+assert { rpn.evaluate('-1') == -1 }
+assert { rpn.evaluate('1 1 +') == (1 + 1) }
+assert { rpn.evaluate('1 -1 +') == 0 }
+assert { rpn.evaluate('2 2 *') == (2 * 2) }
+assert { rpn.evaluate('5 10 -') == (5 - 10) }
+assert { rpn.evaluate('1 2 3 4 + + +') == (1 + 2 + 3 + 4) }
+assert { rpn.evaluate('1 2 + 3 4 + *') == ((2 + 1) * (4 + 3)) }
+assert { rpn.evaluate('20 10 5 4 + * -') == (20 - 10*(5 + 4)) }
 
-
+a, b, c = Array.new(3) { rand(100) }
+assert { rpn.evaluate("#{a} #{b} - #{c} *") == (a-b)*c }
 
 
 # 5. Reflection 
+# This challenge was relatively simple. I'm wondering what the advantages of using assert is over our old driver 
+# tests with printing true/false. The one reason I don't like it right now is because the code stops running once 
+# it does raise an error, but that could probably be fixed by changing the assert method.
--- week_6/2_drawer_debugger/my_solution.rb
+++ week_6/2_drawer_debugger/my_solution.rb
@@ -1,62 +1,71 @@
 # U2.W6: Drawer Debugger
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge Jessica Tatham.
 
+    #Kevin and Jessica
 
 # 2. Original Code
 
 class Drawer
 
-attr_reader :contents
+    attr_reader :contents
 
-# Are there any more methods needed in this class?
+    # Are there any more methods needed in this class?
+        #No
 
-def initialize
-@contents = []
-@open = true
+    def initialize
+        @contents = []
+        @open = true
+    end
+
+    def open
+        @open = true
+    end
+
+    def close
+        @open = false
+    end
+    
+    def add_item(item)
+        @contents << item
+    end
+    
+    def remove_item(item = @contents.pop) #what is `#pop` doing? Taking the item out of the contents variable
+        @contents.delete(item)
+    end
+    
+    def dump  # what should this method return? This method now returns an empty drawer.
+        puts "Your drawer is empty."
+        @contents = []
+    end
+    
+    def view_contents
+        puts "The drawer contains:"
+        @contents.each {|silverware| puts "- " + silverware.type }
+    end
+    
 end
-
-def open
-@open = true
-end
-
-def close
-@open = false
-end 
-
-def add_item
-@contents << item
-end
-
-def remove_item(item = @contents.pop) #what is `#pop` doing?
-@contents.delete(item)
-end
-
-def dump  # what should this method return?
-puts "Your drawer is empty."
-end
-
-def view_contents
-puts "The drawer contains:"
-@contents.each {|silverware| puts "- " + silverware.type }
-end
-
 
 class Silverware
-attr_reader :type
+    attr_reader :type, :clean
 
-# Are there any more methods needed in this class?
+    # Are there any more methods needed in this class?
+        #Yes, we added a clean_silverware method
 
-def initialize(type, clean = true)
-@type = type
-@clean = clean
-end
+    def initialize(type, clean = true)
+        @type = type
+        @clean = clean
+    end
 
-def eat
-puts "eating with the #{type}"
-@clean = false
-end
+    def eat
+        puts "eating with the #{type}"
+        @clean = false
+    end
+    
+    def clean_silverware
+        @clean = true
+    end
 
 end
 
@@ -79,24 +88,39 @@ silverware_drawer.view_contents
 
 removed_knife = silverware_drawer.remove_item(sharp_knife)
 removed_knife.eat
-removed_knife.clean_silverware 
+removed_knife.clean_silverware
 
 silverware_drawer.view_contents
 silverware_drawer.dump
-silverware_drawer.view_contents #What should this return?
+silverware_drawer.view_contents # What should this return? # This should return an empty Drawer
 
 
-fork = silverware_drawer.remove_item(fork) #add some puts statements to help you trace through the code...
+fork = Silverware.new("fork")
+silverware_drawer.add_item(fork)
+silverware_drawer.view_contents
+fork = silverware_drawer.remove_item(fork)
 fork.eat
 
 #BONUS SECTION
-# puts fork.clean
+puts fork.clean
 
 # DRIVER TESTS GO BELOW THIS LINE
 
-
-
-
-
+puts "\nDriver Tests Below:"
+drawer = Drawer.new
+fork = Silverware.new("fork")
+drawer.add_item(fork)
+p drawer.remove_item(fork) == fork
+p drawer.dump == []
+p drawer.open == true
+p drawer.close == false
+p fork.clean == true
+p fork.eat == false
 
 # 5. Reflection 
+# I personally found the instructions in this challenge a bit vague, but I think we did what we were supposed to
+# by adjusting the given method calls and creating/editing methods. We decided to not create assert statements 
+# because it's simpler to do these driver tests with printing a comparison. Maybe when we learn about more complicated 
+# assert statements in the future, then we can apply them more efficiently. One thing that bothered me about this 
+# code is that some of the methods have a puts in the code, so our driver tests have these statements in between 
+# all the trues. Not a big deal, but I hope we can dive into Rspec soon to make better, streamlined tests.
